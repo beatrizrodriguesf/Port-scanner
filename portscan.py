60aefca63c6d234141c2563166a937ab532b1caf
beatrizrodriguesf <beatrizrf1@al.insper.edu.br>
2025-02-27 16:38:55 -0500

    Generalizando funcao port-scan e colocando intervalo de portas personalizavel

--- portscan.py
+++ portscan.py
@@ -4,41 +4,36 @@ import sys, socket, multiprocessing, argparse
 import ipaddress
 
 def port_scan(host_info, porta, family):
-    s = socket.socket(family, socket.SOCK_STREAM)
-    s.settimeout(1)
+
+    with socket.socket(family, socket.SOCK_STREAM) as s:
+        s.settimeout(1)
     
-    if family == socket.AF_INET:
-        x = s.connect_ex((host_info[0], int(porta)))
-    elif family == socket.AF_INET6:
-        x = s.connect_ex((host_info[0], int(porta), host_info[2], host_info[3]))
-    if x == 0:
-        try:
-            service = socket.getservbyport(int(porta), "tcp")
-            print(f"Porta {porta} [TCP] aberta {service}")
-        except OSError:   
-            print(f"Porta {porta} [TCP] aberta")
-
-def net_scan(addr):
-    s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-    socket.setdefaulttimeout(1)
-    x = s.connect_ex((str(addr), 80))
-    if x == 0:
-        return 1
-    else:
+        if family == socket.AF_INET:
+            x = s.connect_ex((host_info[0], int(porta)))
+        elif family == socket.AF_INET6:
+            x = s.connect_ex((host_info[0], int(porta), host_info[2], host_info[3]))
+        if x == 0:
+            return 1
         return 0
-
 
 def main():
 
-    ip_rede = input("Digite o ip ou rede que deseja analisar:").strip()
+    ip_rede = input("Digite o ip ou rede que deseja analisar: ").strip()
 
     if len(ip_rede.split("/")) > 1:
         net = ipaddress.ip_network(ip_rede)
+        print(f"Escaneando rede {ip_rede}")
+
         for ip in net.hosts():
-            if (net_scan(ip)):
-                print(f"IP {ip} is live")
+            info = socket.getaddrinfo(str(ip), None) # Pega ipv6 e ipv4
+            family = info[0][0]
+            host_info = info[0][4]
+
+            if port_scan(host_info, 80, family):
+                print(f"IP {host_info[0]} is live")
+
     else:
-        portas = input("Digite as portas separadas por virgula (deixe em branco para analisar todas):")
+        portas = input("Digite as portas separadas por virgula ou um intervalo separado por hifen: ")
 
         try:
             info = socket.getaddrinfo(ip_rede, None) # Pega ipv6 e ipv4
@@ -49,13 +44,29 @@ def main():
             sys.exit(1)
 
         portas = portas.strip()
+        print(f"Escaneando portas do host {host_info[0]}")
+        print("PORTA - STATUS - SERVICO")
 
-        if portas != "":
+        if len(portas.split("-")) <= 1:
             for p in portas.split(","):
-                port_scan(host_info, p, family)
+                if port_scan(host_info, p, family):
+                    try:
+                        service = socket.getservbyport(int(p), "tcp")
+                        print(f"{p}/tcp - Aberta - {service}")
+                    except OSError:   
+                        print(f"{p}/tcp - Aberta - unknown")
+                    
         else:
-            for p in range(1, 65535):
-                port_scan(host_info, p, family)
+            portas = portas.split("-")
+            inicio = int(portas[0])
+            fim = int(portas[1])
+            for p in range(inicio, fim+1):
+                if port_scan(host_info, p, family):
+                    try:
+                        service = socket.getservbyport(p, "tcp")
+                        print(f"Porta {p} [TCP] aberta {service}")
+                    except OSError:   
+                        print(f"Porta {p} [TCP] aberta")
 
 
 if __name__ == "__main__":
